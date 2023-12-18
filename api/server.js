991f138d11386954c9f0a5d9994f6de1264e4595
Jo-han <jo-han@miloctav.fr>
2023-12-18 16:56:12 +0100

    comment removed

--- api/server.js
+++ api/server.js
@@ -11,27 +11,6 @@ db.sync({
     console.log('SYNC DONE.')
 })
 
-
-// app.use(bodyParser.json({
-//     limit: '20mb'
-// }))
-
-// app.use(bodyParser.urlencoded({
-//     extended: true
-// }))
-
-// // Global middleware to conditionally apply busboy
-// app.use((req, res, next) => {
-//     if (req.headers['Content-Type'] && req.headers['Content-Type'].includes('multipart/form-data')) {
-//         // Busboy need to have a lower case content-type
-//         req.headers['content-type'] = req.headers['Content-Type']
-//         return busboy()(req, res, next)
-//     } else {
-//         // Continue to the next middleware if Content-Type is missing or invalid
-//         return next()
-//     }
-// })
-
 app.get("/", (req, res) => {
     return res.json({
         message: "Projet Artisan API."
